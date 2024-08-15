7bf4f9df1d45bff8414e1d1008e89ea5f4a5ffb0
marcelopetroni <marceloapetroni@hotmail.com>
2024-08-15 19:36:08 -0300

    donation feature in homepage

--- frontend/src/components/DonationBox.jsx
+++ frontend/src/components/DonationBox.jsx
@@ -13,11 +13,11 @@ const DonationBox = ({ onClose }) => {
                 <div className="ends-in">Ends in:</div>
                 <div className="date">mm/dd/yy</div>
             </div>
-            <div className="progress-container">
-                <div className="progress-bar">
-                <div className="progress-filled"></div>
+            <div className="progress-container2">
+                <div className="progress-bar2">
+                <div className="progress-filled2"></div>
                 </div>
-                <div className="progress-values">
+                <div className="progress-values2">
                 <span>US$150</span>
                 <span>US$200</span>
                 </div>
@@ -26,20 +26,23 @@ const DonationBox = ({ onClose }) => {
             <div className="right-section">
             <div className="project-name">Project Name</div>
             <div className="project-owner">Sofia Nogueira</div>
-            <div className="project-description">
-                I'm Sofia Nogueira, a singer and songwriter dedicated to turning emotions into music...
+            <div className="project-description2">
+            I'm Sofia Nogueira, a singer and songwriter dedicated to turning emotions into music. I'm seeking partners and investors to help me launch my debut EP, a project I've been developing with great care and believe has the potential to make a meaningful impact.
             </div>
             <div className="payment-info">Payment Information</div>
             <div className="amount">Amount</div>
             <div className="input-and-button">
-                <input type="text" placeholder="00,00" />
+                <div className="input-wrapper">
+                    <div className="amount-label">US$</div>
+                    <input type="text" placeholder="00,00" />
+                </div>
                 <button className="connect-wallet">Connect Wallet</button>
             </div>
             </div>
         </div>
         <div className="action-buttons">
           <button className="cancel-button" onClick={onClose}>Cancel</button>
-          <button className="donate-button">Donate</button>
+          <button className="donate-button2">Donate</button>
         </div>
       </div>
     </div>
