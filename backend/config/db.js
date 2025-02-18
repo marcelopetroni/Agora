fa4ed2ea32662e8d6efc367ac0d7403bbbe67949
marcelopetroni <marceloapetroni@hotmail.com>
2025-02-17 22:15:20 -0300

    fix: db ajustment

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialect: 'postgres'
 });
 
