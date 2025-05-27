1db0f98ec9b26d47b5715859b5e369f693ab524b
marcelopetroni <marceloapetroni@hotmail.com>
2025-05-26 21:59:29 -0300

    refactor: frontend files change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/landingPage'
-import Home from './pages/homePage';
-import Profile from './pages/profilePage';
-import Social from './pages/socialPage';
-import Project from './pages/projectPage';
-import Info from './pages/infoPage';
+import LandingPage from './views/landingPage'
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/social" element={<Social />} />
-        <Route path="/info" element={<Info />} />
-      </Routes>
-    </Router>
-  );
+return (
+	<Router>
+	<Routes>
+		<Route path="/" element={<LandingPage />} />
+	</Routes>
+	</Router>
+);
 }
 
 export default App;
