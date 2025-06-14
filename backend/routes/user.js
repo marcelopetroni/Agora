6519cc2e5adc0e430d0dad9b141899407e64e4e5
marcelopetroni <marceloapetroni@hotmail.com>
2025-06-14 00:48:29 -0300

    feat: initial setup for google api integration

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -13,6 +13,7 @@ export default class userRoutes {
 	setup() {
 		this.router.get('/', this.UserController.getAllUsers);
 		this.router.post('/login', this.SchemaValidator.validate(UserSchema.login), this.UserController.login);
+		this.router.post('/login-google', this.SchemaValidator.validate(UserSchema.loginGoogle), this.UserController.loginGoogle);
 		this.router.post('/create-user', this.SchemaValidator.validate(UserSchema.create), this.UserController.create);
 		this.router.put('/update-user', this.SchemaValidator.validate(UserSchema.update), this.UserController.update);
 
