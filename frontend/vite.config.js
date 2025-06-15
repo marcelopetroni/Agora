24c52a944a50f1009b596ab7ed21e1faffca4073
Breno do Amaral <brenodoamaral1@gmail.com>
2025-06-15 20:35:31 -0300

    landing page code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,9 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist'
+  },
+  server: {
+    host: true,
+    allowedHosts: ['f71d-2804-214-4011-fc0-1de-5e33-6f0b-cf62.ngrok-free.app'],
   }
 })
